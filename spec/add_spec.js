91aaf110c27be3fe3d5cae180ab6209b7eb5f26e
jenniekibiri <josphatje@gmail.com>
2020-12-14 22:21:08 +0300

    add jasmine test

---
+++ spec/add_spec.js
@@ -0,0 +1,11 @@
+var app=require("../server.js");
+//1.name the test
+//2.give description about the test
+//3.pass params
+//4.do the test 
+describe("Addition",function(){
+it("The function should add 2 numbers",function() {
+var value=app.AddNumber(5,6);
+expect(value).toBe(10);
+});
+});
